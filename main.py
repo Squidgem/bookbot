9a06f8e18b6c4b1a947deac0c25611dae7035fa3
Squidgem <xenocide08@gmail.com>
2025-12-11 15:03:44 -0700

    H: modified the book path to handle arguments submitted via sys.argv with appropriate error and exit codes.

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from stats import get_word_count
 from stats import get_char_count
 from stats import print_report
+import sys
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     text = get_book_contents(book_path)
     words = get_word_count(text)
     print(f"Found {words} total words")
