45f50d998c7afe5af1698e8150e44f4edb187096
Squidgem <xenocide08@gmail.com>
2025-12-11 13:58:39 -0700

    C: building main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_contents(book_path)
+    print(text)
+
+def get_book_contents(path):
+    with open(path) as f:
+        return f.read()
+    
+main()
