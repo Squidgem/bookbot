3a333d1aad13fe3698265394e1e7c47a07d46c4a
Squidgem <xenocide08@gmail.com>
2025-12-11 14:13:18 -0700

    D: built function into main.py to split words and return word count

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_contents(book_path)
-    print(text)
-
+    words = get_word_count(text)
+    print(f"Found {words} total words")
+    
+def get_word_count(text):
+    word_count = 0 
+    for word in text.split():
+        word_count += 1
+    return word_count
+    
 def get_book_contents(path):
     with open(path) as f:
         return f.read()
-    
+
 main()
